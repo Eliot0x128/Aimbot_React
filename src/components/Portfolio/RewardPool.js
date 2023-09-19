ccfcbf855504904148634a0d8b43a52157b0013b
Air128731 <henry.scott0x8080@gmail.com>
2023-09-19 02:17:00 -0400

    portfolio_integration_finished

--- src/components/Portfolio/RewardPool.js
+++ src/components/Portfolio/RewardPool.js
@@ -1,7 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import '../../css/project.css';
+import axios from 'axios';
 
 function RewardPool () {
+    const [buyTaxData, setBuyTaxData] = useState([0, 0, 0, 0]);
+    const [sellTaxData, setSellTaxData] = useState([0, 0, 0, 0]);
+    const [buyPercentData, setBuyPercentData] = useState([0, 0, 0, 0]);
+    const [sellPercentData, setSellPercentData] = useState([0, 0, 0, 0]);
+    const [totalData, setTotalData] = useState([0, 0, 0, 0]);
+    
+    useEffect(() => {
+        const getData = async () => {
+            const response = await axios.get('https://aimbotapi.onrender.com/api/aimbot/profits');
+            const rewardPoolData = response.data;
+            
+            setBuyTaxData([rewardPoolData.day.tax.toFixed(2), rewardPoolData.week.tax.toFixed(2), rewardPoolData.month.tax.toFixed(2), rewardPoolData.total.tax.toFixed(2)]);
+            setSellTaxData([rewardPoolData.day.bot.toFixed(2), rewardPoolData.week.bot.toFixed(2), rewardPoolData.month.bot.toFixed(2), rewardPoolData.total.bot.toFixed(2)]);
+            setBuyPercentData([(rewardPoolData.day.tax / (rewardPoolData.day.tax + rewardPoolData.day.bot) * 100).toFixed(2), (rewardPoolData.week.tax / (rewardPoolData.week.tax + rewardPoolData.week.bot) * 100).toFixed(2), (rewardPoolData.month.tax / (rewardPoolData.month.tax + rewardPoolData.month.bot) * 100).toFixed(2), (rewardPoolData.total.tax / (rewardPoolData.total.tax + rewardPoolData.total.bot) * 100).toFixed(2)]);
+            setSellPercentData([(rewardPoolData.day.bot / (rewardPoolData.day.bot + rewardPoolData.day.bot) * 100).toFixed(2), (rewardPoolData.week.bot / (rewardPoolData.week.bot + rewardPoolData.week.bot) * 100).toFixed(2), (rewardPoolData.month.bot / (rewardPoolData.month.bot + rewardPoolData.month.bot) * 100).toFixed(2), (rewardPoolData.total.bot / (rewardPoolData.total.bot + rewardPoolData.total.bot) * 100).toFixed(2)]);
+            setTotalData([(rewardPoolData.day.tax + rewardPoolData.day.bot).toFixed(2), (rewardPoolData.week.tax + rewardPoolData.week.bot).toFixed(2), (rewardPoolData.month.tax + rewardPoolData.month.bot).toFixed(2), (rewardPoolData.total.tax + rewardPoolData.total.bot).toFixed(2)]);
+        }
+
+        getData();
+    }, []);
 
     return (
         <div className='bg-[#030015] text-white my-24'>
@@ -12,77 +33,77 @@ function RewardPool () {
                     <div className='text-3xl font-bold text-center'>Daily</div>
                     <div className='flex flex-col p-4 mt-6 text-left rounded-md metrics_item'>
                         <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>3.99 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>80.33%</p>
+                        <p className='mt-6 text-xl font-medium'>{sellTaxData[0]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{sellPercentData[0]}%</p>
                     </div>
                 </div>
                 <div className='flex flex-col  w-[300px]'>
                     <div className='text-3xl font-bold'>Weekly</div>
                     <div className='flex flex-col p-4 mt-6 text-left rounded-md metrics_item'>
                         <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>54.01 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>73.00%</p>
+                        <p className='mt-6 text-xl font-medium'>{sellTaxData[1]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{sellPercentData[0]}%</p>
                     </div>
                 </div>
                 <div className='flex flex-col  w-[300px]'>
                     <div className='text-3xl font-bold'>Monthly</div>
                     <div className='flex flex-col p-4 mt-6 text-left rounded-md metrics_item'>
                         <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>170.74 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>48.33%</p>
+                        <p className='mt-6 text-xl font-medium'>{sellTaxData[2]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{sellPercentData[0]}%</p>
                     </div>
                 </div>
                 <div className='flex flex-col  w-[300px]'>
                     <div className='text-3xl font-bold'>Since Launch</div>
                     <div className='flex flex-col p-4 mt-6 text-left rounded-md metrics_item'>
                         <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>205.85 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>49.43%</p>
+                        <p className='mt-6 text-xl font-medium'>{sellTaxData[3]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{sellPercentData[0]}%</p>
                     </div>
                 </div>
             </div>
             <div className='flex flex-col items-center justify-center mt-20 md:mt-2 md:flex-row gap-7'>
                 <div className='flex flex-col w-[300px]'>
                     <div className='flex flex-col p-4 text-left rounded-md metrics_item_down'>
-                        <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>3.99 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>80.33%</p>
+                        <p className='text-sm font-bold'>GENERATED FROM TAX</p>
+                        <p className='mt-6 text-xl font-medium'>{buyTaxData[0]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{buyPercentData[0]}%</p>
                     </div>
                     <div className='flex flex-col mt-4 text-center'>
-                        <p className='text-3xl font-bold'>4.97 ETH</p>
+                        <p className='text-3xl font-bold'>{totalData[0]} ETH</p>
                         <p className='text-xl'>Generated today</p>
                     </div>
                 </div>
                 <div className='flex flex-col w-[300px]'>
                     <div className='flex flex-col p-4 text-left rounded-md metrics_item_down'>
-                        <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>3.99 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>80.33%</p>
+                        <p className='text-sm font-bold'>GENERATED FROM TAX</p>
+                        <p className='mt-6 text-xl font-medium'>{buyTaxData[1]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{buyPercentData[1]}%</p>
                     </div>
                     <div className='flex flex-col mt-4 text-center'>
-                        <p className='text-3xl font-bold'>74.46 ETH</p>
+                        <p className='text-3xl font-bold'>{totalData[1]} ETH</p>
                         <p className='text-xl'>Generated this week</p>
                     </div>
                 </div>
                 <div className='flex flex-col w-[300px]'>
                     <div className='flex flex-col p-4 text-left rounded-md metrics_item_down'>
-                        <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>353.32 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>Generated this month</p>
+                        <p className='text-sm font-bold'>GENERATED FROM TAX</p>
+                        <p className='mt-6 text-xl font-medium'>{buyTaxData[2]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{buyPercentData[2]}%</p>
                     </div>
                     <div className='flex flex-col mt-4 text-center'>
-                        <p className='text-3xl font-bold'>4.97 ETH</p>
-                        <p className='text-xl'>Generated today</p>
+                        <p className='text-3xl font-bold'>{totalData[2]} ETH</p>
+                        <p className='text-xl'>Generated this month</p>
                     </div>
                 </div>
                 <div className='flex flex-col w-[300px]'>
                     <div className='flex flex-col p-4 text-left rounded-md metrics_item_down'>
-                        <p className='text-sm font-bold'>GENERATED FROM AI BOT</p>
-                        <p className='mt-6 text-xl font-medium'>3.99 ETH</p>
-                        <p className='mt-1 text-xl font-medium'>80.33%</p>
+                        <p className='text-sm font-bold'>GENERATED FROM TAX</p>
+                        <p className='mt-6 text-xl font-medium'>{buyTaxData[3]} ETH</p>
+                        <p className='mt-1 text-xl font-medium'>{buyPercentData[3]}%</p>
                     </div>
                     <div className='flex flex-col mt-4 text-center'>
-                        <p className='text-3xl font-bold'>416.40 ETH</p>
+                        <p className='text-3xl font-bold'>{totalData[3]} ETH</p>
                         <p className='text-xl'>Generated since launch</p>
                     </div>
                 </div>
